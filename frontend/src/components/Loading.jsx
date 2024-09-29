a8100794686ff3c033edd4c03475d7fb24d6aadd
ankitjhagithub21 <ajha45075@gmail.com>
2024-09-29 18:30:54 +0530

    added loader

--- frontend/src/components/Loading.jsx
+++ frontend/src/components/Loading.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Loading = () => {
   return (
-    <div className='flex items-center justify-center h-full w-full '>
+    <div className='flex items-center justify-center h-screen w-full '>
         
        <div className="loader"></div>
     </div>
